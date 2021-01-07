5385c89ea3122fc6d160ccb0c439347c2617a420
Abir-Daikhi <dhaiki.abir@future-eye-inc.com>
2021-01-07 16:46:33 +0100

    BackOfficeUser

---
+++ UserBackOffice.Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserBackOffice.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ UserBackOffice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UserBackOffice.MvcApplication" Language="C#" %>
